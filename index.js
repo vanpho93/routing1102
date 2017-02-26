257ec38990e30a88c175ca49f97f1ec0a4331c20
Admin <vanpho02@gmail.com>
2017-02-26 08:47:28 +0700

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+let express = require('express');
+let app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.render('home'));
