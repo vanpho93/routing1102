8dd1d593bbb02775d95d9624109ccb0d232f0e7e
Admin <vanpho02@gmail.com>
2017-02-26 09:19:31 +0700

    base

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 let parser = require('body-parser').urlencoded({extended: false});
 
-router.get('/show', (req, res) => res.send('Hello'));
+router.get('/show', (req, res) => res.render('login'));
 router.post('/show', parser, (req, res) => {
   res.send(req.body);
 });
