e54709478b2d595d0a353a3829c74bd022948fc9
Admin <vanpho02@gmail.com>
2017-03-04 14:29:30 +0700

    create news

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 app.listen(3000, () => console.log('Server started'));
+let parser = require('body-parser').urlencoded({extended: false});
 
 let {mangTin, Tin} = require('./Tin.js');
 
@@ -20,6 +21,9 @@ app.get('/admin', (req, res) => res.render('admin', {mangTin}));
 
 app.get('/admin/news', (req, res) => res.render('add'));
 
-app.post('/admin/news', (req, res) => {
-  res.send('Hello')
+app.post('/admin/news', parser, (req, res) => {
+  let {title, desc, date, image} = req.body;
+  let tin = new Tin(title, date, desc, image);
+  mangTin.push(tin);
+  res.redirect('/admin');
 })
