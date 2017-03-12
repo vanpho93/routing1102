9f3e2e79074e0dedf2534df7a9071deb2d8dfa3f
Admin <vanpho02@gmail.com>
2017-03-12 10:00:19 +0700

    ss

--- index.js
+++ index.js
@@ -71,10 +71,12 @@ app.post('/admin/sua', (req, res) => {
   uploadSingle('avatar')(req, res, err => {
     if(err) return res.send(err);
     let {title, desc, date, index} = req.body;
-    let image = req.file.filename;
+    let image = req.file?req.file.filename:req.body.old;
     let tin = new Tin(title, date, desc, image);
-    mangTin[index] = tin;
-    res.redirect('/admin');
+    updateNews(index, tin, (err, result) => {
+      if(err) return res.send(err);
+      res.redirect('/admin');
+    });
   });
 });
 
